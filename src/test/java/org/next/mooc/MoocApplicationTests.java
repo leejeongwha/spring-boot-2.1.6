e4956781f06a4fc61d3474cc7909dbad514f07d5
leejeongwha <ljk2491@nts-corp.com>
2019-09-30 19:00:41 +0900

    async 서블릿 테스트

--- src/test/java/org/next/mooc/MoocApplicationTests.java
+++ src/test/java/org/next/mooc/MoocApplicationTests.java
@@ -17,11 +17,11 @@ public class MoocApplicationTests {
 	AtomicInteger counter = new AtomicInteger(0);
 	//String url = "http://localhost:8080/test/sync-blocking";
 	
-	//core-size가 8이므로 동시에 8개만 처리
-	//String url = "http://localhost:8080/test/async-blocking"; 
+	//core-size가 8인 경우 동시에 8개만 처리
+	String url = "http://localhost:8080/test/async-blocking"; 
 	
 	//sync-blocking보다 수행시간 절반(서버작업 2개가 별도의 스레드로 동시에 작업 됨)
-	String url = "http://localhost:8080/test/sync-nonBlocking";
+	//String url = "http://localhost:8080/test/sync-nonBlocking";
 
 	/**
 	 * 동시에 100개의 요청을 call(톰캣의 max-threads 디폴트 값은 200)
